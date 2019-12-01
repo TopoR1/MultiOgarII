1b04e5c84dad4807a10c393260667072d5279442
TopoR <egorbelov.com@mail.ru>
2019-12-01 08:37:57 +0300

    var is deprecated

--- src/packet/LeaderboardPosition.js
+++ src/packet/LeaderboardPosition.js
@@ -1,11 +1,11 @@
-var BinaryWriter = require('./BinaryWriter');
+const BinaryWriter = require('./BinaryWriter');
 
 class LeaderboardPosition {
     constructor(position) {
         this.place = position;
     }
     build() {
-        var buf = new BinaryWriter();
+        const buf = new BinaryWriter();
         buf.writeUInt8(0x30);
         buf.writeUInt16(this.place);
         return buf.toBuffer();
