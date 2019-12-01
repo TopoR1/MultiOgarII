3d524d68a1c657ffcd1d6d0e806011589cd52800
TopoR <egorbelov.com@mail.ru>
2019-12-01 08:41:13 +0300

    var is deprecated

--- src/packet/UpdatePosition.js
+++ src/packet/UpdatePosition.js
@@ -1,15 +1,15 @@
-var BinaryWriter = require("./BinaryWriter");
+const BinaryWriter = require("./BinaryWriter");
 
 class UpdatePosition {
     constructor(playerTracker, x, y, scale) {
-        this.playerTracker = playerTracker,
-            this.x = x;
+        this.playerTracker = playerTracker;
+        this.x = x;
         this.y = y;
         this.scale = scale;
     }
 
     build(protocol) {
-        var writer = new BinaryWriter();
+        const writer = new BinaryWriter();
         writer.writeUInt8(0x11);
         writer.writeFloat(this.x + this.playerTracker.scrambleX);
         writer.writeFloat(this.y + this.playerTracker.scrambleY);
